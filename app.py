ce77594aa4fdd3f65cfa12706a499d1fd71e83b5
wade <johnny12150@gmail.com>
2019-06-29 20:49:27 +0800

    Add google map api

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, abort
 import configparser
 import os
+import json
+import requests
+import random
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -24,6 +28,7 @@ config.read("config.ini")
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+google_api_key = os.environ['GOOGLE_API_KEY']
 
 
 @app.route('/')
@@ -50,6 +55,7 @@ def callback():
     return 'ok'
 
 
+# 處理文字訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 使用者輸入的文字
@@ -75,10 +81,79 @@ def handle_message(event):
             event.reply_token,
             buttons_template_message)
 
-    # 如果前面條件都沒觸發，回應使用者輸入的話
+    # 如果前面條件都沒觸發，回應使用者`輸入的話
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=msg))
+
+
+# 處理位置訊息
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+    # 獲取使用者的經緯度
+    lat = event.message.latitude
+    long = event.message.longitude
+    # 使用google API搜尋附近的餐廳
+    nearby_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={}&location={},{}&rankby=distance&type=restaurant&language=zh-TW".format(
+        google_api_key, lat, long)
+    # 得到附近的20家餐廳資訊
+    nearby_results = requests.get(nearby_url)
+    nearby_restaurants_dict = nearby_results.json()
+    top20_restaurants = nearby_restaurants_dict["results"]
+    # 選擇評價>4分的餐聽
+    res_num = (len(top20_restaurants))
+    above4 = []
+    for i in range(res_num):
+        try:
+            if top20_restaurants[i]['rating'] > 3.9:
+                # print('rate: ', top20_restaurants[i]['rating'])
+                above4.append(i)
+        except:
+            KeyError
+
+    if len(above4) < 0:
+        print('no 4 start resturant found')
+        # 隨機選擇一間餐廳
+        restaurant = random.choice(top20_restaurants)
+    restaurant = top20_restaurants[random.choice(above4)]
+    # 4. 檢查餐廳有沒有照片，有的話會顯示
+    if restaurant.get("photos") is None:
+        thumbnail_image_url = None
+    else:
+        # 根據文件，選一張照片
+        photo_reference = restaurant["photos"][0]["photo_reference"]
+        thumbnail_image_url = "https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth=1024".format(
+            google_api_key, photo_reference)
+    # 餐廳詳細資訊
+    rating = "無" if restaurant.get("rating") is None else restaurant["rating"]
+    address = "沒有資料" if restaurant.get("vicinity") is None else restaurant["vicinity"]
+    details = "評分：{}\n地址：{}".format(rating, address)
+
+    # 取得餐廳的 Google map 網址
+    map_url = "https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(
+        lat=restaurant["geometry"]["location"]["lat"],
+        long=restaurant["geometry"]["location"]["lng"],
+        place_id=restaurant["place_id"]
+    )
+
+    # 回覆使用 Buttons Template
+    buttons_template_message = TemplateSendMessage(
+        alt_text=restaurant["name"],
+        template=ButtonsTemplate(
+            thumbnail_image_url=thumbnail_image_url,
+            title=restaurant["name"],
+            text=details,
+            actions=[
+                URITemplateAction(
+                    label='查看地圖',
+                    uri=map_url
+                ),
+            ]
+        )
+    )
+    line_bot_api.reply_message(
+        event.reply_token,
+        buttons_template_message)
 
 
 # 執行flask
