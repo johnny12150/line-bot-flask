511f17f7930b4e3da06da3b8c901c3eead7393bf
wade <johnny12150@gmail.com>
2019-06-30 11:30:07 +0800

    修正查詢空氣品質

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ def handle_message(event):
 
     # 詢問空氣品質 (政府API)
     if "空氣" in msg or "PM2.5" in msg:
-        gov_api = 'https://opendata.epa.gov.tw/api/v1/AQI?%24skip=0&%24top=10&%24format=json'
+        gov_api = 'http://opendata.epa.gov.tw/api/v1/AQI?%24skip=0&%24top=10&%24format=json'
         response = requests.get(gov_api)
         air_data = response.json()
         msg_text1 = air_data[0]['SiteName'] + '空氣品質: ' + air_data[0]['Status']
