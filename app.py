b7899e4d5cc952f768d1568c9804b34abb804fe9
wade <johnny12150@gmail.com>
2019-07-13 22:30:30 +0800

    Add ubereats restaurant list

--- app.py
+++ app.py
@@ -521,9 +521,9 @@ def handle_postback(event):
         location = 'https://www.ubereats.com/zh-TW/feed/?pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMiVFNSU5QyU4QiVFNyVBQiU4QiVFNCVCQSVBNCVFOSU4MCU5QSVFNSVBNCVBNyVFNSVBRCVCOCVFNSU4NSU4OSVFNSVCRSVBOSVFNiVBMCVBMSVFNSU4RCU4MCUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMkNoSUpNVjhrNzFjMmFEUVJtajV5T25fYUtUayUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJnb29nbGVfcGxhY2VzJTIyJTJDJTIybGF0aXR1ZGUlMjIlM0EyNC43ODk0MjY0OTk5OTk5OTglMkMlMjJsb25naXR1ZGUlMjIlM0ExMjEuMDAwMTIwNyU3RA%3D%3D'
         # 透過爬蟲抓出交大可以訂的餐廳
         restaurants = craw_ubereats(location)
-        msg_str = '\n'.join(restaurants[:5])
+
         # 回傳交大可以訂的餐廳
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg_str))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='\n'.join(restaurants[:5])))
 
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text='postback被觸發'))
 
