f1c39530bc17e67171a9a2e058acb4c54f1e45e9
Wade <johnny12150@gmail.com>
2020-02-12 10:31:22 +0800

    fix syntax error

--- app.py
+++ app.py
@@ -425,11 +425,11 @@ def handle_message(event):
         location = 'https://www.ubereats.com/zh-TW/feed/?pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMiVFNSU5QyU4QiVFNyVBQiU4QiVFNCVCQSVBNCVFOSU4MCU5QSVFNSVBNCVBNyVFNSVBRCVCOCVFNSU4NSU4OSVFNSVCRSVBOSVFNiVBMCVBMSVFNSU4RCU4MCUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMkNoSUpNVjhrNzFjMmFEUVJtajV5T25fYUtUayUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJnb29nbGVfcGxhY2VzJTIyJTJDJTIybGF0aXR1ZGUlMjIlM0EyNC43ODk0MjY0OTk5OTk5OTglMkMlMjJsb25naXR1ZGUlMjIlM0ExMjEuMDAwMTIwNyU3RA%3D%3D'
         # 透過爬蟲抓出交大可以訂的餐廳
         restaurants = craw_ubereats(location)
-        reply_header, reply_json = ue_push(event.reply_token, '\n'.join(restaurants[:5])
+        reply_header, reply_json = ue_push(event.reply_token, '\n'.join(restaurants[:5]))
 
         # 回傳交大可以訂的餐廳
         # line_bot_api.reply_message(event.reply_token, TextSendMessage(text='\n'.join(restaurants[:5])))
-        requests.post(line_reply_api, headers=reply_header, json=reply_json)
+        res = requests.post(line_reply_api, headers=reply_header, json=reply_json)
 
     # 如果前面條件都沒觸發，回應使用者輸入的話
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
